3a403d15293baf5de83b8bd5bc62d0f093b6b7b0
Will Carter <wcarter@as-software.com>
2023-07-12 15:24:05 -0500

    feat: refactors error message function, adds typescript, updates vulnerabilities

--- components/Actions.jsx
+++ components/Actions.jsx
@@ -1,5 +1,5 @@
-import config from "../config/config";
-import { numberWithCommas } from "../helpers/utils";
+import config from "../config/config"
+import { numberWithCommas } from "../helpers/utils"
 
 const Actions = ({
   increaseWalletCapacity,
@@ -7,25 +7,47 @@ const Actions = ({
   payDebt,
   debt,
   advanceDay,
-  init
+  init,
+  currentDay,
 }) => {
   return (
     <>
-      <div className='mt-5 flex flex-col max-w-xs'>
-        <div className='flex w-full items-center'>
-          <button className='bg-green-500 px-3 py-1 mr-5 rounded-full' onClick={increaseWalletCapacity}>Buy</button>
-          <p>Wallet Size (+{config.wallet.increase}): ${numberWithCommas(walletExpansionCost)}</p>
+      <div className="mt-5 flex flex-col max-w-xs">
+        <div className="flex w-full items-center">
+          <button
+            className="bg-green-500 px-3 py-1 mr-5 rounded-full"
+            onClick={increaseWalletCapacity}
+          >
+            Buy
+          </button>
+          <p>
+            Wallet Size (+{config.wallet.increase}): $
+            {numberWithCommas(walletExpansionCost)}
+          </p>
         </div>
-        <div className='flex w-full items-center mt-3'>
-          <button className='bg-green-500 px-3 py-1 mr-5 rounded-full' onClick={payDebt}>Pay</button>
+        <div className="flex w-full items-center mt-3">
+          <button
+            className="bg-green-500 px-3 py-1 mr-5 rounded-full"
+            onClick={payDebt}
+          >
+            Pay
+          </button>
           <p>Pay debt: ${numberWithCommas(debt)}</p>
         </div>
       </div>
 
-    <button className='bg-blue-700 px-6 py-4 rounded-full mt-8 mr-5' id="advDay" onClick={advanceDay}>Advance Day</button>
-    <button className='bg-red-700 px-6 py-4 rounded-full' onClick={init}>New Game</button>
+      <button
+        className="bg-blue-700 px-6 py-4 rounded-full mt-8 mr-5"
+        id="advDay"
+        onClick={advanceDay}
+      >
+        {currentDay === config.days - 1 ? "End Round" : "Advance Day"}
+      </button>
+      <button className="bg-red-700 px-6 py-4 rounded-full" onClick={init}>
+        New Game
+      </button>
     </>
   )
 }
 
-export default Actions;
+export default Actions
---
+++ helpers/alerts.ts
@@ -0,0 +1,13 @@
+export enum AlertMessages {
+  NEED_CASH = "You do not have enough cash to purchase this asset",
+  NEED_ASSET = "You do not have any of this asset to sell",
+  NEED_START = "Please click Advance Day to start the game",
+  NEED_WALLET = "You do not have enough wallet space to purchase this asset",
+  NEED_DEBT = "You have already paid off your debt!",
+  NEED_DEBT_CASH = "You do not have enough cash to pay off this debt",
+  LAST_DAY = "Today is your last day! Better sell all your assets!",
+}
+
+export const showAlert = (msg: AlertMessages): void => {
+  alert(msg)
+}
