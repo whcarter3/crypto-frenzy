f07726b580b970c3d86634a016a201f2dcd00cb7
Will Carter <wcarter@as-software.com>
2023-07-14 15:19:28 -0500

    chore: remove statecheck

--- helpers/stateCheck.ts
+++
@@ -1,9 +0,0 @@
-import type { Wallet } from "../lib/types"
-import { showAlert, AlertMessages } from "./alerts"
-
-export const needAsset = (wallet: Wallet, asset: string) => {
-  if (wallet[asset] === 0) {
-    showAlert(AlertMessages.NEED_ASSET)
-    return
-  }
-}
