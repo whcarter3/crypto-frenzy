7e80f97051bd08b0b725452bd48706e173f56c7b
Will Carter <wcarter@as-software.com>
2023-07-18 16:28:33 -0500

    feat: corrects error

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
-import '../styles/globals.css'
-import { AppProps } from 'next/app'
-import { Analytics } from '@vercel/analytics/react'
+import "../styles/globals.css"
+import { AppProps } from "next/app"
+import { Analytics } from "@vercel/analytics/react"
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -8,6 +8,7 @@ function MyApp({ Component, pageProps }: AppProps) {
       <Component {...pageProps} classname="bg-slate-600" />)
       <Analytics />
     </>
+  )
 }
 
 export default MyApp
