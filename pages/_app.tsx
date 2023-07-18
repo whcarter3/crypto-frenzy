3b1350bdbf2c6b0cd4934cb8f437136cee5ebcc9
wcarteras-software <127761220+wcarteras-software@users.noreply.github.com>
2023-07-18 16:29:56 -0500

    feat: Adds vercel analytics (#2)
    * fix: adds typescript
    
    * feat: refactors error message function, adds typescript, updates vulnerabilities
    
    * feat: refactors pay debt logic to it's own file
    
    * more refactoring, buying with multiple assets is bugged
    
    * back to working state
    
    * feat: introduces reducer, adds session high score, adds disable checks for buttons
    
    * fix: removes game.ts for now
    
    * fix: fixes sell buttons
    
    * fix: formatting on high score
    
    * fix: high score and formatting
    
    * fix: fixes log
    
    * adds check if wallet is full
    
    * high score logic buggy, other improvements, new log messages
    
    * changes event messages
    
    * changes config values
    
    * huge refactor, extracting major functions
    
    * chore: remove statecheck
    
    * feat: major refactor complete
    
    * feat: refactors buy function
    
    * feat: refactor handle sell
    
    * feat: changes prices chance
    
    * feat: moves all config to state, adds initial state for all assets, reworks logic to be more generalized
    
    * feat: allows adding of infinite number of assets to initial state
    
    * feat: removes reference to config
    
    * fix: removes uneeded reducer actions
    
    * feat: adds start of logic for easy mode
    
    * feat: adds avg price to transaction
    
    * feat: adds types and comments for all functions, changed log action to accept an array so multiple SET_LOG did not need to be called reducing state updates
    
    * feat: adds vercel analytics
    
    * feat: corrects error

---
+++ pages/_app.tsx
@@ -0,0 +1,14 @@
+import "../styles/globals.css"
+import { AppProps } from "next/app"
+import { Analytics } from "@vercel/analytics/react"
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Component {...pageProps} classname="bg-slate-600" />)
+      <Analytics />
+    </>
+  )
+}
+
+export default MyApp
