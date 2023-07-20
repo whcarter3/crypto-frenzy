cc753a420ae0f499085c1dfd9a79d816d138f7b2
William Carter, III <whcarter3@users.noreply.github.com>
2023-07-20 11:06:41 -0500

    Feature: adds difficulty mode selection to main game (#3)
    * fix: adds typescript
    
    * feat: refactors error message function, adds typescript, updates vulnerabilities
    
    * feat: refactors pay debt logic to it's own file
    
    * more refactoring, buying with multiple assets is bugged
    
    * back to working state
    
    * feat: introduces reducer, adds session high score, adds disable checks for buttons
    
    * fix: removes game.ts for now
    
    * fix: fixes sell buttons
    
    * fix: formatting on high score
    
    * fix: high score and formatting
    
    * fix: fixes log
    
    * adds check if wallet is full
    
    * high score logic buggy, other improvements, new log messages
    
    * changes event messages
    
    * changes config values
    
    * huge refactor, extracting major functions
    
    * chore: remove statecheck
    
    * feat: major refactor complete
    
    * feat: refactors buy function
    
    * feat: refactor handle sell
    
    * feat: changes prices chance
    
    * feat: moves all config to state, adds initial state for all assets, reworks logic to be more generalized
    
    * feat: allows adding of infinite number of assets to initial state
    
    * feat: removes reference to config
    
    * fix: removes uneeded reducer actions
    
    * feat: adds start of logic for easy mode
    
    * feat: adds avg price to transaction
    
    * feat: adds types and comments for all functions, changed log action to accept an array so multiple SET_LOG did not need to be called reducing state updates
    
    * feat: adds vercel analytics
    
    * feat: corrects error
    
    * start of work for game mode
    
    * game mode logic complete
    
    * renames variable to be more clear
    
    * styles changes and mode selection
    
    ---------
    
    Co-authored-by: Will Carter <wcarter@as-software.com>

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import Table from "../components/Table"
 import Actions from "../components/Actions"
 import Header from "../components/Header"
 import Log from "../components/Log"
+import Modal from "../components/GameMode"
 
 export default function Home() {
   const [state, dispatch] = useReducer(reducer, initialState)
@@ -17,17 +18,16 @@ export default function Home() {
         <link rel="icon" href="/favicon1.ico" />
       </Head>
 
-      <main className="prose px-5">
+      <main className="prose px-5 max-w-md">
         <Header state={state} />
 
         <Table state={state} dispatch={dispatch} />
 
-        <Actions
-          dispatch={dispatch}
-          state={state}
-        />
+        <Actions dispatch={dispatch} state={state} />
 
         <Log log={state.log} />
+
+        {state.modalOpen && <Modal state={state} dispatch={dispatch} />}
       </main>
     </div>
   )
